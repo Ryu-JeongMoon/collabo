bbbc1639d94ca882a30c460ba0262e5a4ffae18f
hansol2755 <79893791+hansol2755@users.noreply.github.com>
2021-06-12 16:30:47 +0900

    Add files via upload

---
+++ com/servlet/hansol/mybatis/DBService.java
@@ -0,0 +1,25 @@
+package com.bc.bit.mybatis;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DBService {
+	private static SqlSessionFactory factory;
+	
+	static {
+		try {
+			factory = new SqlSessionFactoryBuilder()
+					.build(Resources.getResourceAsReader("com/bc/bit/mybatis/config.xml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static SqlSessionFactory getFactory() {
+		return factory;
+	}
+}
