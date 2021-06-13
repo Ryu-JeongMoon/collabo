1fa459dcb1f3a1f5ead3edbf408e1255f1666bd8
minsolarkim <77043336+minsolarkim@users.noreply.github.com>
2021-06-13 23:22:04 +0900

    minsol

--- com/servlet/attendance/DAO/DAO.java
+++ com/servlet/attendance/DAO/DAO.java
@@ -17,7 +17,7 @@ import com.servlet.attendance.vo.AttVO;
 
 public class DAO {
 	
-	//게시글(BBS_T)의 전체 건수 조회
+	//게시글의 전체 건수 조회
 	public static int getTotalCount() {
 		SqlSession ss = DBService.getFactory().openSession();
 		int totalCount = ss.selectOne("attendance.totalCount");
