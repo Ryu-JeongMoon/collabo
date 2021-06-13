685fdb189652d793145e3e5c3472e3924d7c9496
hansol2755 <79893791+hansol2755@users.noreply.github.com>
2021-06-13 15:39:18 +0900

    Add files via upload

--- com/servlet/hansol/common/Paging.java
+++ com/servlet/hansol/common/Paging.java
@@ -9,7 +9,7 @@ public class Paging {
 	private int nowBlock = 1; //현재 블록(페이지 담는 단위)
 	
 	private int numPerPage = 10; //하나의 페이지에 표시할 게시글 수
-	private int pagePerBlock = 10; //블록당 표시하는 페이지 갯수
+	private int pagePerBlock = 5; //블록당 표시하는 페이지 갯수
 	
 	private int totalRecord = 0; //총 게시물 갯수(원본 게시글 수)
 	private int totalPage = 0; //전체 페이지 갯수
